75deb2ed20fb32648afbc77f02e1fe3f45994521
Ari Salkow <asalkow@gmail.com>
2021-06-16 19:13:28 +0200

    All files need for Heroku.

---
+++ client.py
@@ -0,0 +1,20 @@
+"""
+Request preditCions from the model as a client would and test teh output
+Author: Jamie Bamforth
+"""
+import requests
+
+
+def check_responses():
+    """Take test features and actual predictions for these features and feed them to the model through the API. Check
+    the predictions from the API match the actual predictions and print 'Success!' if so, else print 'Failure :('"""
+    response_preds = []
+    payload = {'meter_id':200713,
+               'csv_url':'https://raw.githubusercontent.com/Jamie-B22/ITC_final_project/3c6ca44c2db3c56c8de12f46f80a2c5f66baadb6/CP4/200713_weather_feats.csv'}
+    response_preds.append(requests.get('http://127.0.0.1:5000/', params=payload).text)
+    print(response_preds)
+
+
+if __name__ == '__main__':
+    """Import data to feed the model and check the responses from the model API match the actual predictions."""
+    check_responses()
---
+++ inference.py
@@ -0,0 +1,28 @@
+"""
+Implement a trained model on a server using Flask to allow a REST API to be used to get predictions from the model.
+Author: Jamie Bamforth
+"""
+import pandas as pd
+from flask import Flask, request, jsonify
+from flask_prediction_fn import predict_fn
+import os
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def predict_api():
+    meter_id = request.args.get('meter_id') #200713
+    csv_url = request.args.get('csv_url')
+    weather_data_df = pd.read_csv(csv_url, index_col='captured_on_h', parse_dates=True)#str(meter_id) + '_weather_feats.csv', index_col='captured_on_h', parse_dates=True)
+    timestamps, preds = predict_fn(meter_id, weather_data_df)
+    print(timestamps, preds)
+    output = jsonify(timestamps=timestamps, consumption=preds)
+    return output
+
+
+
+if __name__ == '__main__':
+    """Import model, check it is correct model, then run Flask app locally."""
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT')))
